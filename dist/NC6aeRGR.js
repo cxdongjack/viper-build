b1cf2b696741b23b732928bed9ffb5eb8a81cd23
chenxiaodong <chenxiaodong@xiaomi.com>
2017-05-15 14:51:28 +0800

    feat: 打包一个版本

--- dist/NC6aeRGR.js
+++
@@ -1,2 +0,0 @@
-!function(o,n){function e(){return"moduleA"}var u=o.console;u.log("enter module-a"),e(),u.log("success!")}(window);
-//# sourceMappingURL=/tmp/build.js.map
---
+++ dist/_m771LTh.js
@@ -0,0 +1,2 @@
+!function(o){function n(){return"moduleA"}var e=o.console;e.log("enter module-a"),n(),e.log("success!")}(window);
+//# sourceMappingURL=/tmp/build.js.map
